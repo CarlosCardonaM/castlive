1e1e203ae48a7560bf1086b6f270e32e45553be2
Carlos Cardona <carloscardona@Carloss-MacBook-Pro.local>
2025-07-24 14:48:03 -0600

    B1.1 - initial proyect setup and base structure

---
+++ CastLive/CastLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastLiveApp.swift
+//  CastLive
+//
+//  Created by Carlos Cardona on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CastLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
